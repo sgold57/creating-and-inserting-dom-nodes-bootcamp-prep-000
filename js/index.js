129d14dc2a123a690a81f2e2750c00eaa6deab21
carefree-request-1153 <sgold5757@gmail.com>
2020-09-14 20:08:10 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,4 +5,14 @@ element.innerHTML = 'Hello, DOM!';
 element.style.background = '#f9f9f9';
 element.style.textAlign = 'center';
 
+var ul = document.createElement('ul');
+
+for (let i=0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appentChild(ul);
+
 document.body.appendChild(element);
